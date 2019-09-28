c3dc2765ed60d582c57fb5225de1dc2e1d3ef333
Lucas Santos <lhs.santoss@gmail.com>
2019-09-28 19:17:44 -0300

    Add default values

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,10 +5,12 @@ export type AppConfig = typeof config
 export const config = {
   data: {
     donorsChoose: {
-      apiKey: env.get('DATA_DONORSCHOOSE_APIKEY', '')
+      apiKey: env.get('DATA_DONORSCHOOSE_APIKEY', ''),
+      baseApiUri: env.get('DATA_DONORSCHOOSE_BASEAPIURI', 'https://api.donoschoose.org/common/')
     },
     googlePlaces: {
-      apiKey: env.get('DATA_GOOGLEPLACES_APIKEY', '')
+      apiKey: env.get('DATA_GOOGLEPLACES_APIKEY', ''),
+      baseApiUri: env.get('DATA_GOOGLEPLACES_BASEAPIURI', 'https://maps.googleapis.com/maps/api/place/')
     }
   }
 }
