76e820ed53e7a81879af38029c08a0794abbd2bd
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 13:31:26 -0300

    Add List Users interaction

--- src/presentation/CLI/interactions/user/listUsers.ts
+++ src/presentation/CLI/interactions/user/listUsers.ts
@@ -1,5 +1,12 @@
 import { UserService } from '../../../../services/UserService'
+import chalk from 'chalk'
 
 export function listUsers (service: UserService) {
-  return console.log(service.listUsers())
+  const users = service.listUsers()
+  if (!users.length) return console.log(chalk.red('There are no registered users, please register a new user'))
+
+  console.log(chalk.bold.greenBright('These are the currently registered users:'))
+  for (let user of users) {
+    console.log(`${user.id} - ${user.name} <${user.email}> (zipcode: ${user.zipcode})`)
+  }
 }
