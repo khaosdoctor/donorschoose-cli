ad80827a05e3728374fa955f068672034e365864
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 20:21:28 -0300

    Add donors base url config

--- src/presentation/CLI/interactions/config/changeDonorsBaseURL.ts
+++ src/presentation/CLI/interactions/config/changeDonorsBaseURL.ts
@@ -1,5 +1,23 @@
+import inquirer from 'inquirer'
+import { Logger } from '../../../../utils/logger'
 import { DonorsChooseService } from '../../../../services/DonorsChooseService'
 
-export function changeDonorsBaseURL (_service: DonorsChooseService) {
+export async function changeDonorsBaseURL (service: DonorsChooseService, logger: Logger) {
+  const currentConfigs = service.getConfig()
+  let updatedConfig = await inquirer.prompt([
+    {
+      type: 'input',
+      message: 'New base URL (ENTER to leave as it is):',
+      default: currentConfigs.baseApiUri,
+      name: 'baseApiUri'
+    }
+  ])
 
+  const updateObject: typeof currentConfigs = {
+    ...currentConfigs,
+    baseApiUri: updatedConfig.baseApiUri as string
+  }
+
+  await service.updateConfig(updateObject)
+  return logger.success('Base URL changed successfuly!')
 }
