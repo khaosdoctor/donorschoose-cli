f4bebbaa7297277b2be1f3daf02898e7163b5810
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 18:50:40 -0300

    Better listing

--- src/presentation/CLI/interactions/user/listUsers.ts
+++ src/presentation/CLI/interactions/user/listUsers.ts
@@ -1,12 +1,12 @@
+import { Logger } from '../../../../utils/logger'
 import { UserService } from '../../../../services/UserService'
-import chalk from 'chalk'
 
-export function listUsers (service: UserService) {
+export function listUsers (service: UserService, logger: Logger) {
   const users = service.listUsers()
-  if (!users.length) return console.log(chalk.red('There are no registered users, please register a new user'))
+  if (!users.length) return logger.error('There are no registered users, please register a new user')
 
-  console.log(chalk.bold.greenBright('These are the currently registered users:'))
+  logger.success('These are the currently registered users:')
   for (let user of users) {
-    console.log(`${user.id} - ${user.name} <${user.email}> (zipcode: ${user.zipcode})`)
+    logger.message(`${logger.chalk.bold.magenta(user.id)} - ${user.name} <${logger.chalk.yellow(user.email)}> (zipcode: ${user.zipcode})`)
   }
 }
