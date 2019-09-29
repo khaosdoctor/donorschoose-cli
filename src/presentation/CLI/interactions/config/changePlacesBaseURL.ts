59f04aebe2888bf1d7d72945f24ee46fcd4abc50
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 20:21:41 -0300

    add places base url interaction

--- src/presentation/CLI/interactions/config/changePlacesBaseURL.ts
+++ src/presentation/CLI/interactions/config/changePlacesBaseURL.ts
@@ -1,5 +1,23 @@
+import inquirer from 'inquirer'
+import { Logger } from '../../../../utils/logger'
 import { PlacesService } from '../../../../services/PlacesService'
 
-export function changePlacesBaseURL (_services: PlacesService) {
+export async function changePlacesBaseURL (service: PlacesService, logger: Logger) {
+  const currentConfigs = service.getConfig()
+  let updatedConfig = await inquirer.prompt([
+    {
+      type: 'input',
+      message: 'New base URL (ENTER to leave as it is):',
+      default: currentConfigs.baseApiUri,
+      name: 'baseApiUri'
+    }
+  ])
 
+  const updateObject: typeof currentConfigs = {
+    ...currentConfigs,
+    baseApiUri: updatedConfig.baseApiUri as string
+  }
+
+  await service.updateConfig(updateObject)
+  return logger.success('Base URL changed successfuly!')
 }
