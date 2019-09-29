b756ada602b3fcae5f6607c115abc5780ac8910d
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 19:33:44 -0300

    Add active status detection

--- src/presentation/CLI/interactions/user/listUsers.ts
+++ src/presentation/CLI/interactions/user/listUsers.ts
@@ -4,9 +4,12 @@ import { UserService } from '../../../../services/UserService'
 export function listUsers (service: UserService, logger: Logger) {
   const users = service.listUsers()
   if (!users.length) return logger.error('There are no registered users, please register a new user')
+  const activeUser = service.getActiveUser()
+  let activeStatus = ''
 
   logger.success('These are the currently registered users:')
   for (let user of users) {
-    logger.message(`${logger.chalk.bold.magenta(user.id)} - ${user.name} <${logger.chalk.yellow(user.email)}> (zipcode: ${user.zipcode})`)
+    if (activeUser && activeUser.id === user.id) activeStatus = `[${logger.chalk.bold.underline.green('Active')}]`
+    logger.message(`${logger.chalk.bold.magenta(user.id)} - ${user.name} <${logger.chalk.yellow(user.email)}> (zipcode: ${user.zipcode}) ${activeStatus}`)
   }
 }
