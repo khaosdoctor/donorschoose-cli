3da3aa7329860743670c2224d67c814b18b2d38a
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 19:33:50 -0300

    Interaction to list active users

---
+++ src/presentation/CLI/interactions/user/listActive.ts
@@ -0,0 +1,9 @@
+import { Logger } from '../../../../utils/logger'
+import { UserService } from '../../../../services/UserService'
+
+export function listActive (service: UserService, logger: Logger) {
+  const user = service.getActiveUser()
+  if (!user) return logger.error('There are no active users, please select an user in order to list projects')
+  logger.success('Currently active user is:')
+  return logger.message(`${logger.chalk.bold.magenta(user.id)} - ${user.name} <${logger.chalk.yellow(user.email)}> (zipcode: ${user.zipcode})`)
+}
