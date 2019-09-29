541348e3ebda9cefe6ab780c28a2ba0b3bce89be
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 19:07:15 -0300

    Choose user interaction

--- src/presentation/CLI/interactions/user/chooseUser.ts
+++ src/presentation/CLI/interactions/user/chooseUser.ts
@@ -1,5 +1,18 @@
 import { UserService } from '../../../../services/UserService'
+import { Logger } from '../../../../utils/logger'
+import inquirer = require('inquirer')
 
-export function chooseUser (_service: UserService) {
-  return console.log('Choose User')
+export async function chooseUser (service: UserService, logger: Logger) {
+  const users = service.listUsers()
+  if (!users) return logger.error('There are no users registered, please register a new user before selecting')
+
+  const answer = await inquirer.prompt([{
+    type: 'list',
+    message: 'Select an user to be the new active user:',
+    name: 'chosenUser',
+    choices: users.map(user => ({ name: `${user.name} <${logger.chalk.yellow(user.email)}>`, value: user.id }))
+  }])
+
+  await service.selectUser(answer.chosenUser)
+  return logger.success(`User with ID ${logger.chalk.bold.magenta(answer.chosenUser)} is now the active user`)
 }
