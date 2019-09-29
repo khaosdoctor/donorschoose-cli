e719192c9b41439c96dd1b84fef7ad176f1aa4d1
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 20:21:47 -0300

    add places key interaction

--- src/presentation/CLI/interactions/config/changePlacesKey.ts
+++ src/presentation/CLI/interactions/config/changePlacesKey.ts
@@ -1,5 +1,23 @@
+import inquirer from 'inquirer'
+import { Logger } from '../../../../utils/logger'
 import { PlacesService } from '../../../../services/PlacesService'
 
-export function changePlacesKey (_services: PlacesService) {
+export async function changePlacesKey (service: PlacesService, logger: Logger) {
+  const currentConfigs = service.getConfig()
+  let updatedConfig = await inquirer.prompt([
+    {
+      type: 'input',
+      message: 'New API key (ENTER to leave as it is):',
+      default: currentConfigs.apiKey,
+      name: 'apiKey'
+    }
+  ])
 
+  const updateObject: typeof currentConfigs = {
+    ...currentConfigs,
+    apiKey: updatedConfig.apiKey as string
+  }
+
+  await service.updateConfig(updateObject)
+  return logger.success('API key changed successfuly!')
 }
