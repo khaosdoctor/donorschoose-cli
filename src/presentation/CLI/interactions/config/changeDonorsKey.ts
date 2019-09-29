a2f643f35bcc7bbd6e990b9309a50d09edf0c99c
Lucas Santos <lhs.santoss@gmail.com>
2019-09-29 20:21:35 -0300

    Add donors key interaction

--- src/presentation/CLI/interactions/config/changeDonorsKey.ts
+++ src/presentation/CLI/interactions/config/changeDonorsKey.ts
@@ -1,5 +1,23 @@
+import inquirer from 'inquirer'
+import { Logger } from '../../../../utils/logger'
 import { DonorsChooseService } from '../../../../services/DonorsChooseService'
 
-export function changeDonorsKey (_service: DonorsChooseService) {
+export async function changeDonorsKey (service: DonorsChooseService, logger: Logger) {
+  const currentConfigs = service.getConfig()
+  let updatedConfig = await inquirer.prompt([
+    {
+      type: 'input',
+      message: 'New base URL (ENTER to leave as it is):',
+      default: currentConfigs.apiKey,
+      name: 'apiKey'
+    }
+  ])
 
+  const updateObject: typeof currentConfigs = {
+    ...currentConfigs,
+    apiKey: updatedConfig.apiKey as string
+  }
+
+  await service.updateConfig(updateObject)
+  return logger.success('Base URL changed successfuly!')
 }
