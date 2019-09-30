80e8435c82cb53b18c8c84f84b4cdd5bbec76ddc
Lucas Santos <lhs.santoss@gmail.com>
2019-09-30 17:32:04 -0300

    Rename services

--- src/presentation/CLI/interactions/config/listConfig.ts
+++ src/presentation/CLI/interactions/config/listConfig.ts
@@ -2,9 +2,9 @@ import { Logger } from '../../../../utils/logger'
 import { GoogleService } from '../../../../services/GoogleService'
 import { DonorsChooseService } from '../../../../services/DonorsChooseService'
 
-export function listConfig (placesService: GoogleService, donorsService: DonorsChooseService, logger: Logger) {
+export function listConfig (googleService: GoogleService, donorsService: DonorsChooseService, logger: Logger) {
   const config = {
-    places: placesService.getConfig(),
+    google: googleService.getConfig(),
     donors: donorsService.getConfig()
   }
 
@@ -12,8 +12,8 @@ export function listConfig (placesService: GoogleService, donorsService: DonorsC
   logger.warn('Donors Choose config:', 4)
   logger.info(`API Key: ${logger.chalk.bold.magenta(config.donors.apiKey)}`, 6)
   logger.info(`Base API URL: ${logger.chalk.bold.magenta(config.donors.baseApiUri)}`, 6)
-  logger.warn('Google Places config:', 4)
-  logger.info(`API Key: ${logger.chalk.bold.magenta(config.places.apiKey)}`, 6)
-  logger.info(`Base API URL: ${logger.chalk.bold.magenta(config.places.baseApiUri)}`, 6)
+  logger.warn('Google config:', 4)
+  logger.info(`API Key: ${logger.chalk.bold.magenta(config.google.apiKey)}`, 6)
+  logger.info(`Base API URL: ${logger.chalk.bold.magenta(config.google.baseApiUri)}`, 6)
   return
 }
