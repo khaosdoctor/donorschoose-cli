9b014617082ec5bdf00f45ebfb0b896d3df41a73
Lucas Santos <lhs.santoss@gmail.com>
2019-09-30 17:32:11 -0300

    Add list proposals

--- src/presentation/CLI/interactions/proposals/listProposals.ts
+++ src/presentation/CLI/interactions/proposals/listProposals.ts
@@ -1,13 +1,15 @@
+import ora from 'ora'
 import inquirer from 'inquirer'
 import { addUser } from '../user/addUser'
 import { chooseUser } from '../user/chooseUser'
 import { Logger } from '../../../../utils/logger'
 import { APIError } from '../../../../data/errors/APIError'
 import { UserService } from '../../../../services/UserService'
+import { GoogleService } from '../../../../services/GoogleService'
 import { DonorsChooseService } from '../../../../services/DonorsChooseService'
-import { PlacesService } from '../../../../services/PlacesService'
 
-export async function listProposals (service: DonorsChooseService, userService: UserService, placesService: PlacesService, logger: Logger) {
+export async function listProposals (donorsChooseService: DonorsChooseService, userService: UserService, placesService: GoogleService, logger: Logger) {
+  const spinner = ora()
   try {
     if (!userService.listUsers().length) {
       logger.warn('There are no users registered, let us register one...')
@@ -19,23 +21,31 @@ export async function listProposals (service: DonorsChooseService, userService: 
       await chooseUser(userService, logger)
     }
 
-    const results = await service.getProposalsByZipcode()
+    spinner.start('Loading proposals...')
+    const results = await donorsChooseService.getProposalsByZipcode()
     if (!results.metadata.totalProposals) return logger.info('No proposals found')
+    spinner.stop()
 
     const answer = await inquirer.prompt([{
       type: 'rawlist',
       message: 'Select a proposal to learn more about it',
       name: 'selectedProposal',
       choices: results.proposals.map(proposal => ({
-        name: `${logger.chalk.italic(decodeURIComponent(proposal.title))} [${logger.chalk.yellow(proposal.teacherName)}] ${logger.chalk.underline.green(`(${proposal.percentFunded}% complete)`)} - ${logger.chalk.magenta(proposal.schoolName)}, ${proposal.city} - ${proposal.state}, ${proposal.zip}`,
+        name: proposal.getColorizedHeader(logger.chalk),
         value: proposal.id
       }))
     }])
 
-    const proposal = results.proposals.find((proposal) => proposal.id === answer.selectedProposal)
-    return logger.message(proposal)
+    spinner.start('Loading proposal map...')
+
+    const proposal = results.proposals.find((proposal) => proposal.id === answer.selectedProposal)!
+    const map = await placesService.getMap(proposal.latitude, proposal.longitude, proposal.schoolName)
+    proposal.locationMapUrl = map
+
+    spinner.stop()
+    return logger.message(proposal.toColorizedString(logger.chalk))
   } catch (error) {
-    if (error instanceof APIError) return logger.error(error.message)
-    return logger.error(`An unexpected error ocurred: ${error.message}`)
+    if (error instanceof APIError) return spinner.fail(error.message)
+    return spinner.fail(`An unexpected error ocurred: ${error.message}`)
   }
 }
